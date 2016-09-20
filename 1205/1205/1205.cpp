36a06a7c4d29b990462cbb8e1540fad20ff4041f
张世泽 <814135975@qq.com>
2016-09-20 09:56:42 +0800

    add solution of #1205
    add solution of #1205

---
+++ 1205/1205/1205.cpp
@@ -0,0 +1,52 @@
+/*******************************************************************************
+** Copyright (C), 	2016- , Zach Yeo.                                          *
+** File name:		1205.cpp                                                   *
+** Description:                                                                *
+** Author:			Zhang Shize <zszv587@gmail.com>                            *
+**-----------------------------------------------------------------------------*
+** History:                                                                    *
+** v1.0				2016-09-20					                               *
+********************************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int ackerman ( int m, int n );
+
+int main (){
+
+	int m;
+	int n;
+
+	scanf ( "%d%d", &m, &n );
+
+	int res;
+
+	res = ackerman ( m, n );
+
+	printf ( "%d\n", res );
+
+	return 0;
+
+}
+
+int ackerman ( int m, int n ){
+
+	if ( m == 0 ){
+	
+		return n + 1;
+	
+	}
+	else if ( n == 0 ){
+	
+		return ackerman ( m - 1, 1 );
+	
+	}
+	else {
+	
+		return ackerman ( m - 1, ackerman ( m, n - 1 ) );
+	
+	}
+
+}
