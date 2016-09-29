e956836326b8e37cae3e7f63e9f755b4a0079c51
张世泽 <814135975@qq.com>
2016-09-29 10:54:47 +0800

    add solution of #1524
    add solution of #1524

---
+++ 1524/1524/1524.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+** Copyright (C), 	2016- , Zach Yeo.                                          *
+** File name:		1524.cpp                                                   *
+** Description:                                                                *
+** Author:			Zhang Shize <zszv587@gmail.com>                            *
+**-----------------------------------------------------------------------------*
+** History:                                                                    *
+** v1.0				2016-09-29					                               *
+********************************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+/*I love this problem*/
+
+int main ( ){
+
+	int n;
+	scanf ( "%d", &n );
+
+	printf ( "%d/%d\n", n - 1, n );
+
+	return 0;
+
+}
